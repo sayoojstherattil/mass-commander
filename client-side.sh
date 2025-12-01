1d39d28d9cdc3655726590746bafffbdeab883d1
sayooj <sayoojstherattil@gmail.com>
2025-12-01 12:31:26 +0530

    more again

--- client-side.sh
+++ client-side.sh
@@ -4,16 +4,18 @@ set -e
 
 server_ip=$(cat /$USER/mass-commander/permanent-files/server-ip)
 
-mkdir /root/mass-commander/files-from-server/
-cd /root/mass-commander/files-from-server/
+mkdir /root/mass-commander/runtime-files/
+cd /root/mass-commander/runtime-files/
 
 sftp sftpuser@$server_ip <<EOF
 get /data/files-from-server.tar.gz
 exit
 EOF
 
+mkdir /root/mass-commander/files-from-server/
 tar -xzf files-from-server.tar.gz -C /root/files-from-server
 
 source /root/mass-commander/files-from-server/commands-to-run
 
 rm -r /root/mass-commander/files-from-server/
+rm -r /root/mass-commander/runtime-files/
