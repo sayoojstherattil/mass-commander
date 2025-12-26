abddcf683a4f589c5ef920d879ae40f38c284582
sayooj <sayooj@example.com>
2025-12-26 11:37:35 +0530

    package installation method changed

--- scripts/packages-installer.sh
+++ scripts/packages-installer.sh
@@ -1,38 +1,40 @@
-#!/bin/bash -e
+#!/bin/bash
 
-trap 'echo -e "[${BASH_SOURCE}:${LINENO}]\t$BASH_COMMAND" ; read' DEBUG
+snap_package_fetcher() {
+	
+}
+
+snap_package_dealer() {
+	snap_finder_output=$(apt search $package_name | grep -e '$package_name/' -A1 | grep -w snap | awk -F'-> ' '{print $2}' | awk -F' snap' '{print $1}')
+
+	if [ $snap_finder_output -z ]; then
+		snap_package_existence=no
+		
+		installation_method=apt
+	else
+		snap_package_existence=yes
+		snap_package_name=$snap_finder_output
+
+		installation_method=snap
+	fi
+}
+
+user_inputer() {
+	echo "enter the names of packages:"
+
+	while read package_name; do 
+		echo $package_name >> $runtime_files_dir/packages-names-given-by-user
+	done
+}
 
 
-#current_files_in_archive_fetcher
-ls /var/cache/apt/archives > /$USER/mass-commander/runtime-files/files-in-archive-old
+# main
+user_inputer
+snap_package_dealer
 
-echo -ne "enter names of packages one by one: "
-
-while read package_name; do
-	echo "$package_name" >> /$USER/mass-commander/runtime-files/packages-to-install
-done
-
-apt-get install --download-only $(cat /$USER/mass-commander/runtime-files/packages-to-install) -y
-
-#current files_in_archive_fetcher
-ls /var/cache/apt/archives > /$USER/mass-commander/runtime-files/files-in-archive-new
-
-#new_files_in_archive_identifier
-cat /$USER/mass-commander/runtime-files/files-in-archive-new | grep -v -f /$USER/mass-commander/runtime-files/files-in-archive-old > /$USER/mass-commander/runtime-files/fetched-deb-files
-
-mkdir /root/mass-commander/runtime-files/local-repo-creation
-cd /root/mass-commander/runtime-files/
-cp $(cat fetched-deb-files) /root/mass-commander/runtime-files/local-repo-creation
-
-cd /root/mass-commander/runtime-files/local-repo-creation
-dpkg-scanpackages . /dev/null | gzip -9c | tee Packages.gz > /dev/null
-
-echo '/root/mass-commander/permanent-files/temp-sources-file' >> /root/mass-commander/runtime-files/files-to-tar
-echo '/root/mass-commander/runtime-files/packages-to-install' >> /root/mass-commander/runtime-files/files-to-tar
-
-echo 'mv /etc/apt/sources.list /etc/apt/sources.list.bak' >> /root/mass-commander/runtime-files/commands-to-run
-echo 'apt-get update' >> /root/mass-commander/runtime-files/commands-to-run
-echo 'apt-get install $(cat /root/mass-commander/files-from-server/packages-to-install)' >> /root/mass-commander/runtime-files/commands-to-run
-echo 'mv /etc/apt/sources.list.bak /etc/apt/sources.list' >> /root/mass-commander/runtime-files/commands-to-run
-
-/root/mass-commander/scripts/commander.sh
+if [ $installation_method -eq snap ]; then
+	snap_package_fetcher
+	snap_installation_method_command_generator
+elif [ $installation_method -eq apt ]; then
+	apt_installation_method_command_generator
+fi
