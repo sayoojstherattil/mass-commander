4206125259fe119ace885e4ed5494f57ff5b1a51
sayooj <sayooj@example.com>
2026-01-06 17:49:23 +0530

    fixed packages remover

--- server-side/scripts/packages-remover.sh
+++ server-side/scripts/packages-remover.sh
@@ -1,37 +1,62 @@
 #!/bin/bash
 
-apt_package_remover() {
-	echo "enter names of apt packages one by one: "
+default_approach() {
+	packages-verifier.sh
+	status=$(cat $runtime_files_dir/status)
+	if [ $status = 1 ]; then
+		exit 1
+	fi
 
-	while read apt_package_name; do
-		commands-for-clients-to-run.sh "apt remove -y $apt_package_name"
-	done
+	if [ -f $runtime_files_dir/apt-packages ]; then
+		no_of_lines_in_apt_packages_file=$(wc -l $runtime_files_dir/apt-packages | awk -F' ' '{print $1}')
+		if [ $no_of_lines_in_apt_packages_file != 0 ]; then
+			while read apt_package_name; do
+				commands-for-clients-to-run.sh "echo"
+				commands-for-clients-to-run.sh "echo removing apt package $apt_package_name"
+				commands-for-clients-to-run.sh "echo"
+				commands-for-clients-to-run.sh "apt remove -y $apt_package_name"
+			done<$runtime_files_dir/apt-packages
 
-	commands-for-clients-to-run.sh "apt autoremove -y"
+			commands-for-clients-to-run.sh "echo"
+			commands-for-clients-to-run.sh "echo apt cleaning"
+			commands-for-clients-to-run.sh "echo"
+			commands-for-clients-to-run.sh "apt autoremove -y"
+		fi
+	fi
+
+	if [ -f $runtime_files_dir/snap-packages ]; then
+		no_of_lines_in_snap_packages_file=$(wc -l $runtime_files_dir/snap-packages | awk -F' ' '{print $1}')
+		if [ $no_of_lines_in_snap_packages_file != 0 ]; then
+			while read snap_package_name; do
+				commands-for-clients-to-run.sh "echo removing ${snap_package_name}..."
+				commands-for-clients-to-run.sh "snap remove $snap_package_name"
+			done<$runtime_files_dir/snap-packages
+		fi
+	fi
 }
 
 snap_remover() {
 	echo "enter names of snaps one by one: "
 
 	while read snap_package_name; do
-		commands-for-clients-to-run.sh "removing ${snap_package_name}..."
+		commands-for-clients-to-run.sh "echo removing ${snap_package_name}..."
 		commands-for-clients-to-run.sh "snap remove $snap_package_name"
 	done
 }
 
-echo "(a)pt packages"
+echo "(d)efault approach"
 echo "(s)nap packages"
 
-echo -ne "a\ns\n" > $runtime_files_dir/input-options
+echo -ne "d\ns\n" > $runtime_files_dir/input-options
 user-input-validator.sh
 echo
 
 user_input=$(cat $runtime_files_dir/user-input)
 
-if [ $user_input = "s" ]; then
+if [ $user_input = "d" ]; then
+	default_approach
+elif [ $user_input = "s" ]; then
 	snap_remover
-else
-	apt_package_remover
 fi
 
 commander.sh
---
+++ server-side/scripts/packages-verifier.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+packages_distinguisher() {
+	while read package_name; do
+		snap_finder_output=$(grep -e "$package_name/" $runtime_files_dir/apt-search-output -A1 | grep -w snap | awk -F'-> ' '{print $2}' | awk -F' snap' '{print $1}')
+
+		if [ "$snap_finder_output" != "" ]; then
+			echo $snap_finder_output >> $runtime_files_dir/snap-packages
+		fi
+	done<$runtime_files_dir/packages-names-given-by-user
+
+	if [ -f $runtime_files_dir/snap-packages ]; then
+		cat $runtime_files_dir/packages-names-given-by-user | grep -vf $runtime_files_dir/snap-packages > $runtime_files_dir/apt-packages
+	else
+		cp $runtime_files_dir/packages-names-given-by-user $runtime_files_dir/apt-packages	
+	fi
+}
+
+package_searcher() {
+	echo
+	echo "searching for packages..."
+	echo
+	while read package_name; do
+		apt search $package_name >> $runtime_files_dir/apt-search-output 2>/dev/null
+		grep -qe "$package_name/" $runtime_files_dir/apt-search-output
+
+		if [ $? = 0 ]; then
+			echo 0 > $runtime_files_dir/status
+		else
+			echo "no package $package_name found"
+			echo 1 > $runtime_files_dir/status
+		fi
+	done<$runtime_files_dir/packages-names-given-by-user
+}
+
+user_inputer() {
+	echo "enter the names of packages:"
+
+	while read package_name; do 
+		echo $package_name >> $runtime_files_dir/packages-names-given-by-user
+	done
+}
+
+user_inputer
+package_searcher
+packages_distinguisher
