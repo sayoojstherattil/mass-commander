fa978c55cc4a7d8d64994e9d64ae8927cc4059e9
sayooj <sayooj@example.com>
2026-01-06 14:32:40 +0530

    user deletor echo

--- server-side/scripts/user-deletor.sh
+++ server-side/scripts/user-deletor.sh
@@ -3,7 +3,7 @@
 echo "enter usernames one by one: "
 while read username; do
 	commands-for-clients-to-run.sh "userdel $username -r"
-	commands-for-clients-to-run.sh "user $username deleted"
+	commands-for-clients-to-run.sh "echo user $username deleted"
 done
 
 commander.sh
