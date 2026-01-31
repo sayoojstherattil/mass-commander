1de7c5c086b0294b141ebd9fe8092d427b831a6a
sayooj <sayooj@example.com>
2026-01-31 18:12:22 +0530

    sleeping was not there.. added

--- server-side/scripts/systems-rebooter.sh
+++ server-side/scripts/systems-rebooter.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 commands-for-clients-to-run.sh "echo -n 'Rebooting in 3..'"
+commands-for-clients-to-run.sh "sleep 1"
 commands-for-clients-to-run.sh "echo -n '2..'"
+commands-for-clients-to-run.sh "sleep 1"
 commands-for-clients-to-run.sh "echo -n '1..'"
+commands-for-clients-to-run.sh "sleep 1"
 
 commands-for-clients-to-run.sh "systemctl reboot"
--- server-side/scripts/systems-shutdowner.sh
+++ server-side/scripts/systems-shutdowner.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 commands-for-clients-to-run.sh "echo -n 'Shutting down in 3..'"
+commands-for-clients-to-run.sh "sleep 1"
 commands-for-clients-to-run.sh "echo -n '2..'"
+commands-for-clients-to-run.sh "sleep 1"
 commands-for-clients-to-run.sh "echo -n '1..'"
+commands-for-clients-to-run.sh "sleep 1"
 
 commands-for-clients-to-run.sh "systemctl poweroff"
