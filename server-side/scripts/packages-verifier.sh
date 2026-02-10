d2047bfd5ccbdad9d42a995ad941dc097859bd3a
sayooj <sayooj@example.com>
2026-02-10 13:56:48 +0530

    remove file if it is empty; apt and snap

--- server-side/scripts/packages-verifier.sh
+++ server-side/scripts/packages-verifier.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
+empty_file_remover() {
+		file_loc="$1"
+
+		no_of_lines=$(wc -l "$file_loc" | awk -F' ' '{print $1}')
+
+		[ $no_of_lines -eq 0 ] && rm $file_loc
+}
+
 packages_distinguisher() {
 	while read package_name; do
 		snap_finder_output=$(grep -e "$package_name/" $runtime_files_dir/apt-search-output -A1 | grep -w snap | awk -F'-> ' '{print $2}' | awk -F' snap' '{print $1}')
 
 		if [ "$snap_finder_output" != "" ]; then
 			echo $snap_finder_output >> $runtime_files_dir/snap-packages
+#----------------------------------------------------------------------
+# remove snap-packages file if it doesn't contain anything
+#----------------------------------------------------------------------
+			empty_file_remover "$runtime_files_dir/snap-packages"
 		fi
 	done<$runtime_files_dir/packages-names-given-by-user
 
 	if [ -f $runtime_files_dir/snap-packages ]; then
 		cat $runtime_files_dir/packages-names-given-by-user | grep -vf $runtime_files_dir/snap-packages > $runtime_files_dir/apt-packages
+
+#----------------------------------------------------------------------
+# remove apt-packages file if it doesn't contain anything
+#----------------------------------------------------------------------
+		empty_file_remover "$runtime_files_dir/apt-packages"
 	else
 		cp $runtime_files_dir/packages-names-given-by-user $runtime_files_dir/apt-packages	
 	fi
