14de9344dabc51d49dc8445e94ab3bf6b83a4c43
sayooj <sayooj@example.com>
2026-02-22 20:07:06 +0530

    made prompt better

--- server-side/scripts/home-directory-clearer.sh
+++ server-side/scripts/home-directory-clearer.sh
@@ -3,13 +3,13 @@
 systems_reboot_prompt() {
 	echo "rebooting systems is necessary to perform the operation"
 	echo "proceed? (y)/(n)"
-	echo
 
 	echo -ne "y\nn\n" > $runtime_files_dir/input-options
 	user-input-validator.sh
 	user_input=$(cat $runtime_files_dir/user-input)
 
 	if [ "$user_input" = "y" ]; then
+		echo
 		echo -n "be careful not to use the computers until the second reboot which will happen automatically [press enter to continue]"
 		read tmp
 		systems-rebooter.sh
