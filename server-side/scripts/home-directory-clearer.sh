1140a30585c4a5faf679542cab357e5614a08203
sayooj <sayooj@example.com>
2026-02-22 18:45:07 +0530

    username variable expansion issue

--- server-side/scripts/home-directory-clearer.sh
+++ server-side/scripts/home-directory-clearer.sh
@@ -8,11 +8,11 @@ actual_normal_users_finder() {
 
 home_dir_clearer() {
 	commands-for-clients-to-run.sh "while read username; do"
-	commands-for-clients-to-run.sh "	rm -v -rf /home/${username}"
-	commands-for-clients-to-run.sh "	mkhomedir_helper $username"
-	commands-for-clients-to-run.sh "	(cat $mass_commander_base_dir/scripts/profile-last-part | tee -a /home/${username}/.profile) >/dev/null"
-	commands-for-clients-to-run.sh "	chown $username:$username /home/${username}/.profile"
-	commands-for-clients-to-run.sh "	cd /home/${username}"
+	commands-for-clients-to-run.sh "	rm -v -rf /home/\${username}"
+	commands-for-clients-to-run.sh "	mkhomedir_helper \${username}"
+	commands-for-clients-to-run.sh "	(cat $mass_commander_base_dir/scripts/profile-last-part | tee -a /home/\${username}/.profile) >/dev/null"
+	commands-for-clients-to-run.sh "	chown \${username}:\${username} /home/\${username}/.profile"
+	commands-for-clients-to-run.sh "	cd /home/\${username}"
 	commands-for-clients-to-run.sh "	mkdir -v $(cat $permanent_files_dir/default-folders | tr ' ' '\n')"
 	commands-for-clients-to-run.sh "done<$runtime_files_dir_of_client/actual-normal-users"
 }
