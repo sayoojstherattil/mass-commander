0a2ec4fb5d418b5876ce13ca20e891749c70ceb6
sayooj <sayooj@example.com>
2026-02-22 18:37:57 +0530

    moved to scripts

--- server-side/scripts/home-directory-clearer.sh
+++ server-side/scripts/home-directory-clearer.sh
@@ -10,7 +10,7 @@ home_dir_clearer() {
 	commands-for-clients-to-run.sh "while read username; do"
 	commands-for-clients-to-run.sh "	rm -v -rf /home/${username}"
 	commands-for-clients-to-run.sh "	mkhomedir_helper $username"
-	commands-for-clients-to-run.sh "	(cat $mass_commander_base_dir/profile-last-part | tee -a /home/${username}/.profile) >/dev/null"
+	commands-for-clients-to-run.sh "	(cat $mass_commander_base_dir/scripts/profile-last-part | tee -a /home/${username}/.profile) >/dev/null"
 	commands-for-clients-to-run.sh "	chown $username:$username /home/${username}/.profile"
 	commands-for-clients-to-run.sh "	cd /home/${username}"
 	commands-for-clients-to-run.sh "	mkdir -v \"$(cat $permanent_files_dir/default-folders | tr ' ' '\n')\""
