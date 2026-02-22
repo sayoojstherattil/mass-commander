dcd67e5172da981104747831cddfbcecf8850e9e
sayooj <sayooj@example.com>
2026-02-22 15:53:20 +0530

    removed more bugs

--- server-side/scripts/home-directory-clearer.sh
+++ server-side/scripts/home-directory-clearer.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 actual_normal_users_finder() {
-	commands-for-clients-to-run.sh "ls /home | tee -a $runtime_files_dir_of_client/home-dir-folders"
+	commands-for-clients-to-run.sh "ls /home | tee -a $runtime_files_dir_of_client/home-dir-files"
 
-	commands-for-clients-to-run.sh "grep -f $runtime_files_dir_of_client/home-dir-folders /etc/shadow | awk -F':' '{print $1}' > $runtime_files_dir_of_client/actual-normal-users"
+	commands-for-clients-to-run.sh "grep -f $runtime_files_dir_of_client/home-dir-files /etc/shadow | awk -F':' '{print \$1}' > $runtime_files_dir_of_client/actual-normal-users"
 }
 
 home_dir_clearer() {
 	commands-for-clients-to-run.sh "while read username; do"
-		commands-for-clients-to-run.sh "ls -a /home/\${username} > $runtime_files_dir_of_client/contents-of-\${username}"
-		commands-for-clients-to-run.sh "rm -rf '\$(cat $runtime_files_dir_of_client/contents-of-\${username} | tr '\n' ' ')'"
+	commands-for-clients-to-run.sh "	ls -a /home/\${username} > $runtime_files_dir_of_client/contents-of-\${username}"
+	commands-for-clients-to-run.sh "	cat $runtime_files_dir_of_client/contents-of-\${username} | grep -v '\.bashrc' | grep -v '\.profile' | tee $runtime_files_dir_of_client/contents-of-\${username}-other-than-bashrc-and-profile"
+	commands-for-clients-to-run.sh "	rm -rf '\$(cat $runtime_files_dir_of_client/contents-of-\${username}-other-than-bashrc-and-profile | tr '\n' ' ')'"
 
-		commands-for-clients-to-run.sh "while read folder_name; do"
-			commands-for-clients-to-run.sh "mkdir /home/\${username}/$folder_name"
-		commands-for-clients-to-run.sh "done<$permanent_files_dir/default-folders"
+	commands-for-clients-to-run.sh "	while read folder_name; do"
+	commands-for-clients-to-run.sh "		mkdir /home/\${username}/$folder_name"
+	commands-for-clients-to-run.sh "	done<$permanent_files_dir/default-folders"
 	commands-for-clients-to-run.sh "done<$runtime_files_dir_of_client/actual-normal-users"
 }
 
